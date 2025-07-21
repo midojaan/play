b741b8006c4e8810b02b642d017e348af601ce99
arab stram <55789905+midojaan@users.noreply.github.com>
2025-07-21 21:08:31 +0300

    Update access.js

--- css/js/access.js
+++ css/js/access.js
@@ -3,18 +3,28 @@ window.addEventListener("load", () => {
     document.getElementById("access-box").style.display = "flex";
     return;
   }
-  startSplash();
+  // إبدأ السبلاتش على طول لو التفعيل موجود
+  showSplash();
 });
 
 function checkAccess() {
   const input = document.getElementById("access-input");
   const errorMsg = document.getElementById("access-error");
   const correctCode = "114456";
+  
   if (input.value === correctCode) {
     localStorage.setItem("accessGranted", "true");
     document.getElementById("access-box").style.display = "none";
-    startSplash();
+    // إبدأ السبلاتش بعد التفعيل
+    showSplash();
   } else {
     errorMsg.textContent = "تصريح الدخول غير صحيح. حاول مرة أخرى.";
   }
+}
+
+// دالة جديدة للتحكم في السبلاتش
+function showSplash() {
+  const splash = document.getElementById("splash");
+  splash.style.display = "block";
+  startSplash();
 }
